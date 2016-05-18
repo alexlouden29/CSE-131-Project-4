84a4662dd6f07d1e5ed9fcb2fd37d9e68e961da7
Betty Louie <cs131sbc@ieng6-201.ucsd.edu>
2016-05-17 20:59:35 -0700

    changed stuff in symtable

--- symtable.cc
+++ symtable.cc
@@ -2,4 +2,76 @@
  * Symbol table implementation
  *
  */
+#include "symtable.h"
+#include "ast.h"
+#include "ast_type.h"
+#include "ast_decl.h"
 
+SymbolTable::SymbolTable(){
+
+  llvm::Value* llvmValue = NULL;
+
+  map<string, Decl*> s1;
+
+  vector<scope> scopess;
+  scopes = scopess;
+  scopes.push_back(s1);
+
+  ifFlag = false;
+  elseFlag = false;
+  elifFlag = false;
+  whileFlag = false;
+  forFlag = false;
+  globalScope = false;
+  funcFlag = false;
+  doWhileFlag = false;
+  switchFlag = false;
+  breakFlag = false;
+  returnFlag = false;
+  caseFlag = false;
+  defaultFlag = false;
+  Type *returnType = NULL;
+}
+
+void SymbolTable::pushScope(scope *s){
+  scopes.push_back(*s);
+}
+
+void SymbolTable::popScope(){
+  scopes.pop_back();
+}
+
+void SymbolTable::addSymbol(string key, Decl* decl){
+  scope* m = &scopes.back();
+  m->insert(pair<string,Decl*>(key,decl));
+}
+
+Decl* SymbolTable::lookup(string key){
+  Decl* d = NULL;
+  for(vector<scope>::reverse_iterator vectorIt = scopes.rbegin(); vectorIt != scopes.rend(); ++vectorIt){
+    scope* s = &(*vectorIt);
+    d = lookupInScope(key, s);
+    if(d != NULL){
+      break;
+    }
+  }
+  return d;
+}
+
+Decl* SymbolTable::lookupInScope(string key, scope *s){
+  int i = s->count(key);
+  if(i > 0){
+    return s->at(key);
+  }
+  return NULL;
+}
+
+scope* SymbolTable::currScope(){
+  if (&scopes.back() == NULL){
+  }
+  return &(scopes.back());
+}
+
+int SymbolTable::size(){
+  return scopes.size();
+}
--- symtable.h
+++ symtable.h
@@ -4,3 +4,51 @@
  *  Header file for Symbol table implementation.
  */
 
+#ifndef _H_symtable
+#define _H_symtable
+
+#include <vector>
+#include <map>
+#include <string>
+#include "ast_decl.h"
+
+#include "ast.h"
+#include "llvm/IR/Value.h"
+
+#include "irgen.h"
+
+using namespace std;
+typedef map<string, Decl*> scope;
+
+class SymbolTable {
+  protected:
+    vector<scope> scopes;
+
+  public:
+    SymbolTable(); //constructor
+
+    llvm::Value* llvmValue;
+
+    bool ifFlag, elseFlag, elifFlag;
+    bool whileFlag, forFlag;
+    bool globalScope;
+    bool funcFlag;
+    bool doWhileFlag; 
+    bool switchFlag, caseFlag, defaultFlag;
+    bool breakFlag;
+    bool returnFlag;
+    Type *returnType;
+
+    void pushScope(scope *s);
+    void popScope();
+    void addSymbol(string key, Decl* decl);
+    Decl* lookup(string key);
+    scope* currScope();
+    Decl* lookupInScope(string key, scope *s);
+    int size();
+   
+};
+
+
+#endif
+
